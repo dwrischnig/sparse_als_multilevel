4a128d584b29d60f5a0eb145774b7e168e1ebeba
Philipp Trunschke <philipp.trunschke@univ-nantes.fr>
2023-05-07 11:56:36 +0200

    allow using podman

--- run_sampler.sh
+++ run_sampler.sh
@@ -1,18 +1,24 @@
 #! /bin/bash
 
-cd problem/
+# CONTAINER=docker
+CONTAINER=podman
 NAME=run_sampler
-EXEC="docker exec ${NAME}"
-docker start ${NAME} &>/dev/null
-if [ $? -ne 0 ]; then
-    docker run --name ${NAME} -dit -v $(pwd):/home/fenics/shared -w /home/fenics/shared quay.io/fenicsproject/stable:current
-    ${EXEC} pip install --user joblib
+
+${CONTAINER} start ${NAME} &>/dev/null
+CONTAINER_EXISTS=$?
+
+set -e
+
+cd problem/
+if [ ${CONTAINER_EXISTS} -ne 0 ]; then
+    ${CONTAINER} run --name ${NAME} -dit -v $(pwd):/home/fenics/shared -w /home/fenics/shared quay.io/fenicsproject/stable
+    ${CONTAINER} exec ${NAME} pip install --user joblib
 fi
 
 for VARIANCE in 2 5 10; do
-    ${EXEC} python3 parametric_pde_sampling/compute_samples.py -b 100 darcy_lognormal_${VARIANCE} 20000
-    ${EXEC} python3 parametric_pde_sampling/compute_functional.py -f identity darcy_lognormal_${VARIANCE}
+    ${CONTAINER} exec ${NAME} python3 parametric_pde_sampling/compute_samples.py -b 100 darcy_lognormal_${VARIANCE} 20000
+    ${CONTAINER} exec ${NAME} python3 parametric_pde_sampling/compute_functional.py -f identity darcy_lognormal_${VARIANCE}
 done
 
-${EXEC} python3 parametric_pde_sampling/compute_samples.py -b 100 darcy_rauhut 20000
-${EXEC} python3 parametric_pde_sampling/compute_functional.py -f integral darcy_rauhut
+${CONTAINER} exec ${NAME} python3 parametric_pde_sampling/compute_samples.py -b 100 darcy_rauhut 20000
+${CONTAINER} exec ${NAME} python3 parametric_pde_sampling/compute_functional.py -f integral darcy_rauhut
