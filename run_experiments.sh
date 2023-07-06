804980ecf48ed350b1f3576df3b6fc5b0414f4e4
Philipp Trunschke <philipp.trunschke@univ-nantes.fr>
2023-07-06 23:24:42 +0200

    feat: perform experiments for different rates

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 
 for t in 50 100 500 1000; do
-    python run.py darcy_rauhut ${t} 1000 -q integral -a sals
-    python run.py darcy_rauhut ${t} 1000 -q integral -a tensap
-    python run.py darcy_rauhut ${t} 1000 -q integral -a ssals
+    python run.py problem/darcy_rauhut ${t} 1000 -q integral -a sals
+    python run.py problem/darcy_rauhut ${t} 1000 -q integral -a tensap
+    python run.py problem/darcy_rauhut ${t} 1000 -q integral -a ssals
 done 
 
-for k in 2_0 4_0; do
-    for t in 50 100 500 1000; do
-        python run.py darcy_lognormal_${k} ${t} 1000 -q pod_mode -a sals
-        python run.py darcy_lognormal_${k} ${t} 1000 -q pod_mode -a tensap
-        python run.py darcy_lognormal_${k} ${t} 1000 -q pod_mode -a ssals
-    done 
+for RATE in 1 2; do
+    for VARIANCE in 2_0 4_0; do
+        for t in 50 100 500 1000; do
+            python run.py problem/darcy_lognormal_rate_${RATE}/${VARIANCE} ${t} 1000 -q pod_mode -a sals
+            python run.py problem/darcy_lognormal_rate_${RATE}/${VARIANCE} ${t} 1000 -q pod_mode -a tensap
+            python run.py problem/darcy_lognormal_rate_${RATE}/${VARIANCE} ${t} 1000 -q pod_mode -a ssals
+        done
+    done
 done
